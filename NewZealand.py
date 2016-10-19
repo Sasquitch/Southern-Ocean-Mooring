fde8ac6d3162a8c2628f63472afb2c1a5632c1aa
Sasquitch <Davpasquale@gmail.com>
2016-10-19 15:28:36 -0600

    Data stores to variables. Added abbv dictionary
    Variable and Attribute data still needs to be implemented

--- NewZealand.py
+++ NewZealand.py
@@ -1,6 +1,6 @@
 import os
 from os.path import isfile, join
-from pandas import read_csv
+#from pandas import read_csv
 from netCDF4 import Dataset
 import numpy as np
 #from numpy import arange, dtype # array module from http://numpy.scipy.org
@@ -19,6 +19,8 @@ def getData(linenum, toStr): #analyze data in line
 		print("getData received bad data.")
 
 dataDict = dict()
+abbvDict = {'time': 'date', 'dirt': 'dirt', 'spe': 'speed', 'tem': 'temperature',
+			'pre': 'pressure', 'dep': 'depth', 'con': 'conductivity', 'sal': 'salinity'}
 
 
 nc = Dataset('test.nc', 'w', format = 'NETCDF4')
@@ -33,14 +35,19 @@ with open(fileChoice) as f:
 	content = [content.rstrip('\n') for content in open(fileChoice)]
 
 
-for i in range(0,30): #find line where data begins
+for i in range(0,30): #find line where data begins SHOULD NOT BE PAST 30
 	temp_str = content[i]
 	if temp_str[:4] == 'time': #Does it always begin with time?
 		variables = content[i].split(" ") #insert into array
 		variables = list(filter(('').__ne__, variables)) #remove blank entries
+		for y in range(0, len(variables)):
+			try:
+				variables[y] = abbvDict[variables[y]]
+			except:
+				print("There is an unknown abbreviation: \'", variables[y],"\'\nConsider adding its description to the dictionary at the top of the python file:\n", __file__)
 		dataLine = i
 
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#XXXXXXXXXXXXX Read file header information XXXXXXXXXXXXXXXXX
 
 instrInfo = getData(1,0) #get instrument data
 instrType = instrInfo[0]
@@ -71,47 +78,44 @@ timeZone = datetimeinfo[4]
 notes = ''
 for x in range(7, dataLine):
 	lineInfo = getData(x,1)
-	if lineInfo.isnumeric() == True:
-		print (lineInfo)
+	if lineInfo.isnumeric() == True: #skip empty lines
+		break #Do nothing
 	else:
 		lineInfo = lineInfo.split(str(x+1))[0]
-		notes = notes + "\n" + lineInfo
+		notes = notes + "\n" + lineInfo #Unsure if \n is the correct formatting
 
-print (notes)
+#XXXXXXXXXXXXX end read file header information XXXXXXXXXXXXXXXXXXXXXXXXXX
 
+variables.insert(1,"datetime")
 
-#CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-variables.insert(1,"HHMMSS")
-entries = len(variables)
-
-
-for ii in range(0,entries):
+for ii in range(0,len(variables)):
 	dataDict.update({ii: []}) #create dictionary matching vars to empty arrays
  
+
 for j in range(dataLine+1, len(content)):
 	data = getData(j,0)
-	for jj in range(0, entries): #loop over entries in data array 
-		dataDict[jj].append(data[jj])
+	for jj in range(0, len(variables)): #loop over entries in data array and store in dictionary
+		clean = ''.join(c for c in data[jj] if c not in '-:') #numpy cannot analyze non numbers
+		dataDict[jj].append(clean)
 
 #print(dataDict)
 
 
 for iii in range(0,len(variables)): #netcdf algorithm
 
+	nc.createDimension(variables[iii], len(dataDict[iii])-1) #pair variable name to data len dimension
 
-	nc.createDimension(variables[iii], len(dataDict[iii])) #pair variable name to data len dimension
-
+	#print(nc.dimensions[variables[iii]])
 	dataList = np.array(dataDict[iii]).tolist()
-	#tempVar = np.arange(dataList)
+
 	tempVar = dataList
 	#print(dataDict[iii])
 
 	tempVar[:] = dataDict[iii]
 	#print (tempVar[:])
-	tempStr = variables[iii]
-	tempStr = nc.createVariable(tempStr, np.float32, (tempStr,))
-	#print(tempStr)
+	foo = nc.createVariable(variables[iii], np.float32, (variables[iii],))
+	foo.units = dataDict[iii][0]
+	foo[:] = dataDict[iii][1:]
+	#print  (foo.units)
 
-temp = nc.createVariable('temp', np.float32, (variables))
-#print  (nc.dimensions['HHMMSS'])
 nc.close()
